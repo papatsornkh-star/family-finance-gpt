29283e4b11551ad81ea4c6206e71b6c4870ea44d
papatsornkh-star <papatsorn.kh@gmail.com>
2026-08-18 13:15:12 +0700

    Add EmptyState component for displaying empty states

---
+++ components/EmptyState.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+
+type EmptyStateProps = {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  buttonText?: string;
+  onClick?: () => void;
+};
+
+export default function EmptyState({
+  icon,
+  title,
+  description,
+  buttonText,
+  onClick
+}: EmptyStateProps) {
+  return (
+    <div className="empty-state">
+      <div className="empty-icon">
+        {icon}
+      </div>
+
+      <div className="empty-title">
+        {title}
+      </div>
+
+      <div className="empty-description">
+        {description}
+      </div>
+
+      {buttonText && (
+        <button
+          className="button button-secondary"
+          style={{ marginTop: 14 }}
+          onClick={onClick}
+        >
+          + {buttonText}
+        </button>
+      )}
+    </div>
+  );
+}
